1a95499a691db25fc4ec71078b51b7f59c512fac
Victor Pham <vbp8vgc@virginia.edu>
2020-02-04 18:36:48 -0500

    Implemented interactive graph, logging data to file

--- data.py
+++ data.py
@@ -1,15 +1,26 @@
 #handle and store data from xbee here
 import random
+from datetime import datetime
 
 class Info(object):
     def __init__(self):
-        self.mph = random.randint(10,20),
+
+        self.mph = random.randint(10,20);
         self.rpm = random.randint(10,20),
-        self.miles = random.randint(10,20),
+        self.miles = random.randint(10,20);
+        self.socTime = str(datetime.now().strftime("%H:%M:%S")),
+        self.socVal = random.randint(10,200);
+
 
     def to_json(self):
         return {
             "mph": self.mph,
             "rpm": self.rpm,
             "miles": self.miles,
+            "socTime": self.socTime,
+            "socVal": self.socVal,
+
         }
+    
+    def change(self):
+        self.mph = random.randint(10,200);
