3ed67d118960e2ccb5af3dc1f6917f57d7f5c540
Victor Pham <vbp8vgc@virginia.edu>
2020-02-25 17:48:37 -0500

    Updated client.js

--- static/js/client.js
+++ static/js/client.js
@@ -5,11 +5,80 @@ socket.on('connect', function() {
 })
 
 socket.on('dataEvent', function(data) {
-  $('#mph').text(data.mph)
-  $('#rpm').text(data.rpm)
-  $('#miles').text(data.miles)
-  console.log(data.socTime);
-  addData(myLineChart, data.socTime, data.socVal);
+  //Main Dashboard
+  $('#current').text(data.ba)
+  $('#voltage').text(data.bb)
+  $('#').text(data.bc)
+  $('#maxTemperature').text(data.bd)
+  $('#temperature').text(data.be)
+  $('#').text(data.bf)  //charge_limit
+  $('#').text(data.bg)  //discharge_limit
+  $('#').text(data.bh)
+
+  $('#').text(data.ca)
+  $('#').text(data.cb)
+  $('#').text(data.cc)
+  $('#').text(data.cd)
+  $('#').text(data.ce)
+  $('#').text(data.cf)
+  $('#').text(data.cg)
+  $('#').text(data.ch)
+  
+  $('#').text(data.fa)
+  $('#').text(data.fb)
+  $('#').text(data.fc)
+  $('#').text(data.fd)
+  $('#').text(data.fe)
+  $('#').text(data.ff)
+  $('#').text(data.fg)
+  $('#').text(data.fh)
+  $('#').text(data.fi)
+  $('#').text(data.fj)
+  $('#').text(data.fk)
+  $('#').text(data.fl)
+  $('#').text(data.fm)
+  $('#').text(data.fn)
+  $('#').text(data.fo)
+  $('#').text(data.fp)
+  $('#').text(data.fq)
+  $('#').text(data.fr)
+  $('#').text(data.fs)
+  $('#').text(data.ft)
+  $('#').text(data.fu)
+
+  $('#').text(data.kg)
+  $('#').text(data.kh)
+
+  $('#').text(data.sa)
+  $('#').text(data.sb)
+  $('#').text(data.sc)
+  $('#').text(data.sd)
+  $('#').text(data.se)
+  $('#').text(data.sf)
+  $('#').text(data.sg)
+  $('#').text(data.sh)
+
+
+  $('#rpm').text(data.ka)
+  $('#mph').text(data.ka * 7 * 60 / 5280)
+  $('#').text(data.kb)
+  $('#').text(data.kc)
+  $('#').text(data.kd)
+  $('#').text(data.ke)
+  $('#').text(data.kf)
+
+  $('#').text(data.t)
+
+
+  //state of charge chart
+  addData(myLineChart, data.t, data.bc);
+
+  //Motor
+
+
+  
+
+
   
   socket.emit('dataEvent', "Data Received")
 })
