a99ee4e83813cbaa4eb32bf78304ac05006710e5
Victor Pham <vbp8vgc@virginia.edu>
2020-02-29 20:46:12 -0500

    Implemented local storage

--- static/js/client.js
+++ static/js/client.js
@@ -2,9 +2,50 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('connect', function() {
   socket.emit('dataEvent', "User Connected")
-})
+  getStoredData();
+});
 
 socket.on('dataEvent', function(data) {
+  console.log(data);
+  displayData(data);
+
+  //local storage 
+  localStorage.setItem("data", JSON.stringify(data));
+
+  
+
+
+  if(localStorage.getItem("graphData") != undefined) {
+    var gd = JSON.parse(localStorage.getItem("graphData"));
+    var val1 = data.socVal;
+    gd.push(val1);
+    localStorage.setItem("graphData", JSON.stringify(gd));
+  }
+  else{
+    var graphData = [];
+    var val1 = data.socVal;
+    graphData.push(val1);
+    localStorage.setItem("graphData", JSON.stringify(graphData));
+  }
+
+  if(localStorage.getItem("time") != undefined) {
+    var time = JSON.parse(localStorage.getItem("time"));
+    var val1 = data.socTime;
+    time.push(val1);
+    localStorage.setItem("time", JSON.stringify(time));
+  }
+  else{
+    var time = [];
+    var val1 = data.socTime;
+    time.push(val1);
+    localStorage.setItem("time", JSON.stringify(time));
+  }
+
+  
+  socket.emit('dataEvent', "Data Received")
+})
+
+function displayData(data){
   //Main Dashboard
   $('#current').text(data.ba)
   $('#voltage').text(data.bb)
@@ -69,16 +110,26 @@ socket.on('dataEvent', function(data) {
 
   $('#').text(data.t)
 
-
-  //state of charge chart
-  addData(myLineChart, data.t, data.bc);
-
-  //Motor
+    //state of charge chart
+    //addData(myLineChart, data.t, data.bc);
 
 
-  
+  //old data
+  $('#rpm').text(data.rpm);
+  $('#mph').text(data.mph);
+  addData(myLineChart,data.socTime, data.socVal);
+}
 
+function getStoredData(){
+    data = JSON.parse(localStorage.getItem("data"));
+    displayData(data);
 
-  
-  socket.emit('dataEvent', "Data Received")
-})
+    //update graph
+    var soc = JSON.parse(localStorage.getItem("graphData"));
+    var time = JSON.parse(localStorage.getItem("time"));
+    
+    for(var i = 0; i < soc.length; i++){
+      addData(myLineChart, time[i], soc[i]);
+    }
+ 
+}
