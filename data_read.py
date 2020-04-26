8388df8d892c438e6493ed0705657440ad9d9dae
Victor Pham <vbp8vgc@virginia.edu>
2020-04-25 20:13:40 -0400

    Fixed data_read.py

--- data_read.py
+++ data_read.py
@@ -11,6 +11,7 @@ serial_port = 'ttyS11'
 ser = serial.Serial(serial_port, 115200, timeout=1)
 
 while True:
-	s = ser.read(135)
+	s = ser.read(104)
 	print(s)	
 	print(msgpack.unpackb(s,raw=False))
+	print("\n\n")
