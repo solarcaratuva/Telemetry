baf50444e3b1d8678a5ec054b97021637252ff59
jayakumarPawan <120random.things@gmail.com>
2021-10-24 17:44:14 -0400

    added credentials file to keep them private

--- config.py
+++ config.py
@@ -1,22 +1,12 @@
 import os
-
+from credentials import creds
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-POSTGRES = {
-    'user': 'postgres',
-    'pw': '1234',
-    'db': 'telemetry',
-    'host': 'localhost',
-    'port': '5432',
-}
+
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'vnkdjnfjknfl1232#')
-
-
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-
-    BASIC_AUTH_USERNAME = 'byoon'
-    BASIC_AUTH_PASSWORD = '123'
+    SECRET_KEY = os.environ.get('SECRET_KEY', creds['key'])
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % creds
+    SQLALECHMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(creds['user'],
+                            creds['pw'], creds['host'], creds['port'], creds['db'])
