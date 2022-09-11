5583629909f5708e754da997721b1c3becf56134
Ja-Zhua <ajd8xe@virginia.edu>
2022-09-11 17:34:52 +0000

    server.py

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import socketio from "socket.io-client";
+import { SOCKET_URL } from "config";
+
+export const socket = socketio.connect(SOCKET_URL);
+export const SocketContext = React.createContext();
 
 function App() {
   return (
