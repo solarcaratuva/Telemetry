98ca08eba4c6db5cbd0f30c511722e1bd945a6b2
RoryGudka <rdgudka@gmail.com>
2022-10-09 13:02:25 -0400

    Track backend

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.info("a user connected");
+  socket.join("connections");
+  socket.on("disconnect", (reason) => {
+    console.info(`User has disconnected for reason: ${reason}`);
+  });
+});
+
+server.listen(5000, () => {
+  console.info("WSS listening on http://localhost:5000");
+});
+
+/**
+ * This is an example of sending data over the websocket.
+ * Feel free to add more intervals and labels other than "message" to use for different features
+ */
+
+setInterval(() => {
+  //Random integer between 0 and 100
+  const number = Math.floor(Math.random() * 100);
+  const timestamp = new Date().toISOString();
+  io.to("connections").emit("message", {
+    timestamp,
+    number,
+  });
+}, 1000);
