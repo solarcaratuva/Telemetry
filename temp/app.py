8a51d1ad5844c401612a43240055b6d64aa584b2
Ethan Ermovick <eeermo@gmail.com>
2023-01-22 12:42:21 -0500

    started json

--- temp/app.py
+++ temp/app.py
@@ -2,15 +2,11 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from random import randint
 import time
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "b"
-socket = SocketIO(app)
-
-# Route for seeing a data
-@app.route("/")
-def main():
-    return render_template("index.html")
+socket = SocketIO(app, cors_allowed_origins="*")
 
 @socket.on('message')
 def handle(msg):
@@ -19,6 +15,14 @@ def handle(msg):
     socket.send(mph)
     print("IWRHFHWGOWIHRHOHGW")
 
+@socket.on("connect")
+def connectHandle():
+    print("connected")
+    time.sleep(10)
+    current_date = datetime.now()
+    socket.send("pedal_value", {timestamp: current_date.isoformat(), number: 50})
+    print("send date")
+
 # Running app
 if __name__ == '__main__':
-    socket.run(app)
+    socket.run(app, port=5050, allow_unsafe_werkzeug=True)
