8b905eef236c6eb0846cf562b178a413c488579e
Ethan Ermovick <eeermo@gmail.com>
2023-01-29 12:22:32 -0500

    added alertbox

--- frontend/src/pages/InteractivePage.tsx
+++ frontend/src/pages/InteractivePage.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import OnePedalDrive from "../components/OnePedalDrive";
 import { io } from "socket.io-client";
 import ToggleButtons from "../components/ToggleButtons";
+import AlertBox from "../components/AlertBox";
 
 const socket = io("http://localhost:5050");
 const MAX_LENGTH = 50;
@@ -221,7 +222,7 @@ const InteractivePage = () => {
                     height: "calc(25vh - 8px)",
                   }}
                 >
-                  <Typography>Motor Faults</Typography>
+                    <AlertBox data={["Alert 1", "Alert2"]}/>
                 </Paper>
                 {/* Replace this paper component with fifa chart */}
                 <Paper
