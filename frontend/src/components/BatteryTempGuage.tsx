4dbbe6afaf55f2078f0df004be20c4ff3021fd77
Ethan Ermovick <eeermo@gmail.com>
2023-02-12 12:48:30 -0500

    removed unused imports

--- frontend/src/components/BatteryTempGuage.tsx
+++ frontend/src/components/BatteryTempGuage.tsx
@@ -1,4 +1,4 @@
-import {Box, Paper, Typography } from "@mui/material";
+import {Box, Typography } from "@mui/material";
 
 import React from "react";
 import ReactSpeedometer from "react-d3-speedometer";
--- frontend/src/pages/HeadsUpPage.tsx
+++ frontend/src/pages/HeadsUpPage.tsx
@@ -1,4 +1,4 @@
-import {Box, Paper, Typography} from "@mui/material";
+import {Box} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import VideoFeed from "../components/VideoFeed";
 import OnePedalDrive from "../components/OnePedalDrive";
