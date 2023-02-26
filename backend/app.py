18e969244f00afafd10fbcacf46bc0a226fcc793
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2023-02-25 21:08:46 -0500

    PingPong method in app.py, doesn't work

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
-import random
 import serial
 import eventlet
 import socketio
 from datetime import datetime
-import time, threading
+import time
 
 
 
@@ -12,62 +11,14 @@ app = socketio.WSGIApp(sio)
 ser = serial.Serial(port="/dev/serial0")
 print("Listening on: "+ser.name)
 
-#val = ser.read(100).decode('utf-8')
-#val = random.randint(1, 100)
+@sio.on('message')
+def handle(msg):
+    message = ser.read(100)
+    message = message.decode('utf-8')
+    sio.send(message)
+    print("MESSAGE SENT: "+message)
+    time.sleep(1)
 
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
-
-
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
-
-def main():
-    message = random.randint(1,100)
-    sio.emit(message)
-    print("MESSAGE SENT: "+str(message))
-    sio.sleep(8)
-    message = random.randint(1,100)
-    sio.emit(message)
-    print("MESSAGE SENT: "+str(message))
-    sio.sleep(8)
-    message = random.randint(1,100)
-    sio.emit(message)
-    print("MESSAGE SENT: "+str(message))
-    sio.sleep(8)
-
-t1 = threading.Thread(target=main, name='t1')
-t1.start()
-print("THREAD STARTED")
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5050)), app)
-
-"""
-import serial, socketio, json, time
-
-ser = serial.Serial(port="/dev/serial0")
-sio = socketio.Server()
-
-# PySerial testing
-print("Listening on: "+ser.name)
-while True:
-    message = ser.read(100).decode('utf-8')
-    print(message)
-    print("MESSAGE RECIEVED")
-
-
-while True:
-    data = serial.read(1000).decode('utf-8')
-    message = decode_can_dbc(0, data)
-    json = json.dumps(message)
-    sio.emit("event", json)
-   
-    time.sleep(0.01)
-"""
