3f7f4a8cf6e8e0855961b2b8bb33d0f1a95ef5eb
Ethan Ermovick <eeermo@gmail.com>
2023-04-23 14:25:23 -0400

    example throttle

--- backend/CANtesting.py
+++ backend/CANtesting.py
@@ -9,6 +9,9 @@ import cantools
 ser = serial.Serial(port="/dev/serial0", invert=True)
 
 bpsDB = cantools.database.load_file('backend/CAN-messages/BPS.dbc')
-message_frame_id = 1062
-encoded_message = bpsDB.encode_message(message_frame_id, {'low_temperature': 25, 'low_thermistor_id': 1, "high_temperature": 55, "high_thermistor_id": 2})
+message_frame_id = 513
+ex_dict = {"throttle": 100, "regen": 100, "cruise_control_speed": 0, "cruise_control_en": 0, "forward_en": 0, "reverse_en": 0, "motor_on": 0}
+# encoded_message = bpsDB.encode_message(message_frame_id, {'low_temperature': 25, 'low_thermistor_id': 1, "high_temperature": 55, "high_thermistor_id": 2})
+# ser.write(encoded_message)
+encoded_message = bpsDB.encode_message(message_frame_id, ex_dict)
 ser.write(encoded_message)
