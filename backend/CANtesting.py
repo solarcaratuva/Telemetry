b8c27449c95924f44d3bac1e095609925991a412
casscientist <casscientist@gmail.com>
2023-05-07 13:19:59 -0400

    made app send radio

--- backend/CANtesting.py
+++ backend/CANtesting.py
@@ -7,7 +7,6 @@ import cantools
 
 # Testing
 ser = serial.Serial(port="/dev/serial0", invert=True)
-
 bpsDB = cantools.database.load_file('backend/CAN-messages/Rivanna2.dbc')
 message_frame_id = 513
 ex_dict = {"throttle": 100, "regen": 100, "cruise_control_speed": 0, "cruise_control_en": 0, "forward_en": 0, "reverse_en": 0, "motor_on": 0}
