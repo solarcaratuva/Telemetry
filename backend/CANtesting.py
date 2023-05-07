b4eff16a0d7c6e503a3890b4a01116a02e9f9fc5
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2023-05-07 14:34:30 -0400

    app.py works, but isn't complete

--- backend/CANtesting.py
+++ backend/CANtesting.py
@@ -18,10 +18,11 @@ def append_int_as_le_bytes(input_bytes, integer):
 
 
 # Testing
+randomVal = random.randint(10, 100)
 ser = serial.Serial(port="/dev/serial0", invert=True)
 bpsDB = cantools.database.load_file('backend/CAN-messages/Rivanna2.dbc')
 message_frame_id = 513
-ex_dict = {"throttle": 100, "regen": 100, "cruise_control_speed": 123, "cruise_control_en": 1, "forward_en": 0, "reverse_en": 0, "motor_on": 0}
+ex_dict = {"throttle": randomVal, "regen": 100, "cruise_control_speed": 123, "cruise_control_en": 1, "forward_en": 0, "reverse_en": 0, "motor_on": 0}
 # encoded_message = bpsDB.encode_message(message_frame_id, {'low_temperature': 25, 'low_thermistor_id': 1, "high_temperature": 55, "high_thermistor_id": 2})
 # ser.write(encoded_message)
 data = bpsDB.encode_message("ECUMotorCommands", ex_dict)
--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,11 @@ sio = socketio.Server(cors_allowed_origins=["http://localhost:3000"])
 app = socketio.WSGIApp(sio)
 ser = serial.Serial(port="/dev/serial0")
 
+# Lists of frames for each applicable CAN message
+CANframes = {"ECUPowerAuxCommands": ['hazards', 'brake_lights', 'headlights', 'left_turn_signal', 'right_turn_signal'],
+            "ECUMotorCommands": ['throttle']} #, 'regen', 'cruse_control_speed', 'cruise_control_en', 'forward_en', 'reverse_en', 'motor_on']}
+# ... more to come
+
 
 def exit_handler():
     print("Closing serial port")
@@ -22,8 +27,28 @@ atexit.register(exit_handler)
 
 isRunning = False
 
+def sendData():     # replacement for send_data
+    print("LISTENING FOR DATA")
+    while True:
+        encoded_message = ser.read(64)
+        timestamp = datetime.now().isoformat()
+        message_id = int.from_bytes(encoded_message[:4], byteorder="little")
+        name, values = decode_dbc(message_id, encoded_message[4:-1])
+        if name not in CANframes:
+            print("INVALID")
+            continue
+        correctFrames = CANframes.get(name)
+        message = []
+        for data in correctFrames:
+            sio.emit("pedal_value", {"timestamp": timestamp, "number": values[data]})
+            print("DATA: " + data + " " + str(values[data]))
+        #sio.emit(message)
+        # XBEE radio also send message
+        sio.sleep(1)
 
-def send_data():
+
+
+def send_data(): # to be deleted once 'sendData()' is done
     while True:
         encoded_message = ser.read(64)
         current_date = datetime.now()
@@ -42,7 +67,7 @@ def connect(sid, environ):
     global isRunning
     if not isRunning:
         isRunning = True
-        sio.start_background_task(send_data)
+        sio.start_background_task(sendData)
 
 
 if __name__ == '__main__':
