a33de8f0abe0b8d2426ce9c6e94fbfdebf9372a7
Ethan Ermovick <eeermo@gmail.com>
2023-06-03 21:06:27 -0400

    sdlkf

--- backend/test_set_time.py
+++ backend/test_set_time.py
@@ -6,11 +6,10 @@ def set_system_time(epoch_seconds):
     dt = datetime.datetime.fromtimestamp(epoch_seconds)
 
     # Format the date and time in the format expected by the date command
-    # date_str = dt.strftime('%m%d%H%M%Y.%S')
-
+    date_str = dt.strftime('%y%m%d%H%M.%S')
     # Call the date command to set the system time
     # This must be run with sudo permissions
-    subprocess.call(["date", f"""$(date -r {epoch_seconds} +'%y%m%d%H%M.%S')"""])
+    subprocess.call(["sudo", "date", "-s", date_str])
 
 if __name__ == "__main__":
     set_system_time(1000)
