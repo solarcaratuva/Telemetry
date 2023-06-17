c24fd7424fe23785539db0b41d7d8a79e3692afe
Ethan Ermovick <eeermo@gmail.com>
2023-06-17 18:14:00 -0400

    update file names

--- backend/decode_can_dbc.py
+++ backend/decode_can_dbc.py
@@ -1,13 +1,18 @@
+import os
+
 import can
 import cantools
 from pathlib import Path
 
 
 def decode_dbc(message_id, message_data): #message_id -> frame_id, message_data -> binary representation of the message data
-    bpsDB = cantools.database.load_file('backend/CAN-messages/BPS.dbc')
-    motorControllerDB = cantools.database.load_file('backend/CAN-messages/MotorController.dbc')
-    mpptDB = cantools.database.load_file('backend/CAN-messages/MPPT.dbc')
-    rivanna2DB = cantools.database.load_file('backend/CAN-messages/Rivanna2.dbc')
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    can_dir = os.path.join(curr_path, "CAN-messages")
+
+    bpsDB = cantools.database.load_file(os.path.join(can_dir, "BPS.dbc"))
+    motorControllerDB = cantools.database.load_file(os.path.join(can_dir, "MotorController.dbc"))
+    mpptDB = cantools.database.load_file(os.path.join(can_dir, "MPPT.dbc"))
+    rivanna2DB = cantools.database.load_file(os.path.join(can_dir, "Rivanna2.dbc"))
 
     #Testing
     # data = bpsDB.messages
