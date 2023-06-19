2cd10e2e6d0e1ca26d6615e5cd6cba072ce598ae
Ethan Ermovick <eeermo@gmail.com>
2023-06-19 14:32:50 -0400

    fix car.py to send

--- backend/car.py
+++ backend/car.py
@@ -1,19 +1,21 @@
-#Runs on the PI, takes data off ECU board over UART, parses CAN and publishes to server
+# Runs on the PI, takes data off ECU board over UART, parses CAN and publishes to server
 import atexit
 import os
-import serial,time
+
 import cantools
 import eventlet
+import serial
 import socketio
 
+from backend import Config
 from send_from_can import CANSender, get_xbee_connection
 
 # XBee Mac addresses
 # pit - 0013A20041C4ACC3
 # car - 0013A20041C4AC5F
 
-#USB port on PI (UART splitter)
-ser = serial.Serial("/dev/ttyUSB0",9600)
+# USB port on PI (UART splitter)
+ser = serial.Serial("/dev/ttyUSB0", 9600)
 sio = socketio.Server(cors_allowed_origins=["http://localhost:3000"])
 app = socketio.WSGIApp(sio)
 # ser = serial.Serial(port="/dev/serial0")
@@ -22,7 +24,7 @@ curr_path = os.path.dirname(os.path.abspath(__file__))
 can_dir = os.path.join(curr_path, "CAN-messages")
 # Lists of frames for each applicable CAN message
 CANframes = {"BPSError": cantools.database.load_file(os.path.join(can_dir, "BPS.dbc")).get_message_by_name(
-                 "BPSError").signal_tree,
+    "BPSError").signal_tree,
              "MotorControllerError": cantools.database.load_file(
                  os.path.join(can_dir, "MotorController.dbc")).get_message_by_name("MotorControllerError").signal_tree,
              "PowerAuxError": cantools.database.load_file(os.path.join(can_dir, "Rivanna2.dbc")).get_message_by_name(
@@ -34,8 +36,8 @@ CANframes = {"BPSError": cantools.database.load_file(os.path.join(can_dir, "BPS.
              "MotorControllerPowerStatus": ["motor_rpm"]
              }
 
-
-device = get_xbee_connection()
+if Config.USE_RADIO:
+    device = get_xbee_connection()
 
 
 def exit_handler():
@@ -47,7 +49,7 @@ def exit_handler():
 
 atexit.register(exit_handler)
 
-sender = CANSender(sio, CANframes) #from send_from_can.py
+sender = CANSender(sio, CANframes)  # from send_from_can.py
 
 isRunning = False
 
@@ -60,11 +62,11 @@ isRunning = False
 def sendData():
     while True:
         encoded_message = ser.read(1)
-        start_byte = int.from_bytes(encoded_message,"big") #Checks for start byte as int for beginning of message
-        if(start_byte == 249): #249 is the start message byte
-            encoded_message += ser.read(24) #read rest of 25 byte message
-            sender.send(encoded_message) #Send data to be parsed to CAN
-            device.send_data_broadcast(encoded_message) #Send over radio to Telemetry
+        start_byte = int.from_bytes(encoded_message, "big")  # Checks for start byte as int for beginning of message
+        if start_byte == 249:  # 249 is the start message byte
+            encoded_message += ser.read(24)  # read rest of 25 byte message
+            sender.send(encoded_message)  # Send data to be parsed to CAN
+            device.send_data_broadcast(encoded_message)  # Send over radio to Telemetry
             sio.sleep(1)
 
 
@@ -75,36 +77,30 @@ def connect(sid, environ):
         isRunning = True
         sio.start_background_task(sendData)
 
+
 time_received = False
 if __name__ == '__main__':
-    while True:
-        encoded_message = ser.read(1)
-        start_byte = int.from_bytes(encoded_message,"big") #Checks for start byte as int for beginning of message
-        if(start_byte == 249): #249 is the start message byte
-            encoded_message += ser.read(24) #read rest of 25 byte message
-            sender.send(encoded_message) #Send data to be parsed to CAN
-            #device.send_data_broadcast(encoded_message) #Send over radio to Telemetry
-            sio.sleep(1)
     # pit starts by looping time messages
-    # Car comes on, recieves time message
-    # Sends acknnoledgement
-    # Pit receives ack, sneds back ack
-    # Car recives ack and starts transmitting data
-    def time_handler(msg):
-        global time_received
-        if time_received:
-            return
-        msgtxt: str = msg.data.decode("utf8")
-        if msgtxt.startswith("Time:"):
-            seconds = int(msgtxt[5:])
-            os.system(f"sudo date -s '@{seconds}'")
-            time_received = True
-            #device.del_data_received_callback(time_received)
-            #device.send_data_broadcast("ack")
+    # Car comes on, receives time message
+    # Sends acknowledgement
+    # Pit receives ack, sends back ack
+    # Car receives ack and starts transmitting data
+    if Config.USE_RADIO:
+        def time_handler(msg):
+            global time_received
+            if time_received:
+                return
+            msgtxt: str = msg.data.decode("utf8")
+            if msgtxt.startswith("Time:"):
+                seconds = int(msgtxt[5:])
+                os.system(f"sudo date -s '@{seconds}'")
+                time_received = True
+                device.del_data_received_callback(time_received)
+                device.send_data_broadcast("ack")
 
 
-    #device.add_data_received_callback(time_handler)
-    while not time_received:
-        pass
-    exit(0)
+        device.add_data_received_callback(time_handler)
+        while not time_received:
+            pass
+
     eventlet.wsgi.server(eventlet.listen(('localhost', 5050)), app)
