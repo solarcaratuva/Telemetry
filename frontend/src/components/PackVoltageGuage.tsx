0e8531cd3e7dee43d50ed0784d0a7fa1580282f5
Ethan Ermovick <eeermo@gmail.com>
2023-06-21 16:18:19 -0400

    update expected value for pack voltage

--- frontend/src/components/PackVoltageGuage.tsx
+++ frontend/src/components/PackVoltageGuage.tsx
@@ -17,7 +17,7 @@ const CurrentGuage: React.FC<Props> = ({ voltage }) => {
             </Typography>
             <Box>
                 {
-                    <ReactSpeedometer ringWidth={40} minValue={0} maxValue={40} customSegmentStops={[0, 15, 18, 22, 25, 40]} segmentColors={["#fc312f", "#f0ff55", "#2ffc31", "#f0ff55","#fc312f"]} textColor="black" value={voltage} />
+                    <ReactSpeedometer ringWidth={40} minValue={0} maxValue={150} customSegmentStops={[0, 120, 130, 140, 150]} segmentColors={["#fc312f", "#f0ff55", "#2ffc31", "#f0ff55","#fc312f"]} textColor="black" value={voltage} />
                 }
             </Box>
         </Box>
