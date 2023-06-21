b80cbf08805977ad79f2db1922b11b1b57a1adde
Ethan Ermovick <eeermo@gmail.com>
2023-06-21 16:21:02 -0400

    update current

--- frontend/src/components/NetCurrentGuage.tsx
+++ frontend/src/components/NetCurrentGuage.tsx
@@ -14,7 +14,7 @@ const CurrentGuage: React.FC<Props> = ({ current, darkMode}) => {
   return (
     <Box flex="1 0 0">
       <Typography variant="h5" color ={darkMode ? "white" : "black"} pb={1}>
-        Net Current (Solar)
+        Net Current
       </Typography>
       <Box>
         {
