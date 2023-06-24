d93d5dc3088bc9a32148a625e5d9caad48e93b48
Ethan Ermovick <eeermo@gmail.com>
2023-06-24 00:32:02 -0400

    update reading

--- Test/serialtest.cpp
+++ Test/serialtest.cpp
@@ -1,19 +1,41 @@
 #include <boost/asio.hpp>
 #include <iostream>
+#include <fstream>
 
-#define SERIAL_PORT "/dev/serial0" // Replace with your serial port
 #define BAUD_RATE 9600 // Replace with your baud rate
 
-int main() {
-    boost::asio::io_service io;
-    boost::asio::serial_port serial(io, SERIAL_PORT);
+std::string get_available_port() {
+    std::ifstream check_port;
 
+    check_port.open("/dev/ttyUSB0");
+    if(check_port) {
+        return "/dev/ttyUSB0";
+    }
+
+    check_port.close();
+    check_port.open("/dev/ttyUSB1");
+    if(check_port) {
+        return "/dev/ttyUSB1";
+    }
+
+    return "";
+}
+
+int main() {
+    std::string port = get_available_port();
+    if(port.empty()) {
+        std::cerr << "No available port found" << std::endl;
+        return 1;
+    }
+
+    boost::asio::io_service io;
+    boost::asio::serial_port serial(io, port);
     serial.set_option(boost::asio::serial_port_base::baud_rate(BAUD_RATE));
 
     char c;
     while (true) {
         boost::asio::read(serial, boost::asio::buffer(&c,1));
-        std::cout << c;
+        std::cout << (int) c << "\n";
     }
 
     return 0;
