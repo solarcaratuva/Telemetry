08b96e12a0518eb7ec38637bdba3cd827063fa93
Ethan Ermovick <eeermo@gmail.com>
2023-06-23 22:40:52 -0400

    update import

--- Test/SerialTest.py
+++ Test/SerialTest.py
@@ -3,7 +3,10 @@ import atexit
 import serial.tools.list_ports
 import serial
 
-from backend.send_from_can import get_can_data
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
+from send_from_can import get_can_data
 
 
 def get_serial_connection():
