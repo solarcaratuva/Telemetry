e7af0a2d893d5a3277c7f42c6a9fa3fca8824d96
Ethan Ermovick <keg9ve@virginia.edu>
2024-05-01 23:27:23 -0400

    update logger to filter only errors

--- backend/serial_logger.py
+++ backend/serial_logger.py
@@ -13,7 +13,19 @@ while True:
         if start_byte == 249:  # 249 is the start message byte
             encoded_message += ser.read(24)
             print(encoded_message)
-            print(get_can_data(encoded_message))
+            name, values = get_can_data(encoded_message)
+            if name in ("BPSError", "MotorControllerError", "PowerAuxError"):
+                for k, v in values.items():
+                    # if v == 1:
+                    print(f"{name}: {k}: {v}")
+                # errors = []
+                # for data in self.can_messages[name]:
+                #     if values[data]:
+                #         errors.append(data)
+                #
+                # # print("ERRORS: " + str(errors))
+                # self.sio.emit(name, {"timestamp": timestamp, "array": errors})
+                # return True
     except Exception as e:
         print(f"error: {e}")
 
