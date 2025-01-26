cefe607e7167e4aa7d1ca18b99b4fdb4d511ddac
Luke <lukinator30000@gmail.com>
2025-01-26 15:14:23 -0500

    Please actually push the data_dispay.py stuff

---
+++ sd_card/data_display.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+sd_data = open("sample.txt", "r") # Using sample text file to demonstrate streamlit displaying file data 
+
+st.title("SD Card")
+
+st.subheader("The following is sample data from the sd card")
+
+stuff = sd_data.read()
+
+st.info(stuff)
