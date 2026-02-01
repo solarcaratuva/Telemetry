0e5fbde88a73160b7fb2365d74230fcaf1277d84
Lukinator3000 <Lukinator30000@gmail.com>
2026-02-01 15:02:03 -0500

    Updated autoAT for LTE implementation

--- autoAT.py
+++ autoAT.py
@@ -1,27 +1,8 @@
-# Copyright (c) 2019, Digi International, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from umqtt.simple import MQTTClient
 import network
 import time
 import xbee
+from machine import Pin
 
 # --- Static AWS Parameters ---
 HOST = b'a14ezejktp3brt-ats'
@@ -43,105 +24,40 @@ TOPIC = "solarcar/us-east-1/car1/telemetry"
 def enterRegion(newRegion):
     REGION = newRegion
 
-def publish_test(message, region=REGION, client_id=CLIENT_ID, host_prefix=HOST, sslp=SSL_PARAMS):
-
-    # Build the AWS endpoint dynamically using the region parameter
-    # This ensures it works even if you pass a normal string
-    if isinstance(region, str):
-        region = region.encode('utf-8')
-    
-    hostname = b'%s.iot.%s.amazonaws.com' % (host_prefix, region)
-
-    # Connect to AWS.
-    client = MQTTClient(client_id, hostname, ssl=True, ssl_params=sslp)
-    print("- Connecting to AWS... ", end="")
-    client.connect()
-    print("[OK]")
-    
-    # Publish the parameterized message.
-    print("- Publishing message... ", end="")
-    message_json = '{"message": "%s"}' % message
-    client.publish(TOPIC, message_json)
-    print("[OK]")
-    
-    # Disconnect.
-    client.disconnect()
-    print("- Done")
-
-
-def sub_cb(topic, msg):
-    """
-    Callback executed when messages from subscriptions are received. Prints
-    the topic and the message.
-
-    :param topic: Topic of the message.
-    :param msg: Received message.
-    """
-
-    global msgs_received
-
-    msgs_received += 1
-    print("- Message received!")
-    print("   * %s: %s" % (topic.decode("utf-8"), msg.decode("utf-8")))
-
-
-def subscribe_test(region, client_id=CLIENT_ID, sslp=SSL_PARAMS,
-                   msg_limit=2):
-    """
-    Connects to AWS, subscribes to a topic and starts listening for messages.
-
-    :param region: region specified for hostname
-    :param client_id: Unique identifier for the device connected.
-    :param hostname: AWS hostname to connect to.
-    :param sslp: SSL certificate parameters.
-    :param msg_limit: Maximum number of messages to receive before
-        disconnecting from AWS..
-    """
-
-    global msgs_received
-
-    hostname = b'%s.iot.%s.amazonaws.com' % (HOST, region)
-
-    # Connect to AWS.
-    client = MQTTClient(client_id, hostname, ssl=True, ssl_params=sslp)
-    client.set_callback(sub_cb)
-    print("- Connecting to AWS... ", end="")
-    client.connect()
-    print("[OK]")
-    # Subscribe to topic.
-    print("- Subscribing to topic '%s'... " % TOPIC, end="")
-    client.subscribe(TOPIC)
-    print("[OK]")
-    # Wait for messages.
-    msgs_received = 0
-    print('- Waiting for messages...')
-    while msgs_received < msg_limit:
-        client.check_msg()
-        time.sleep(1)
-    # Disconnect.
-    client.disconnect()
-    print("- Done")
-
-
 
 print(" +--------------------------------------+")
 print(" | XBee MicroPython AWS Publish Message |")
 print(" +--------------------------------------+\n")
+
+xb = xbee.XBee()
 
 conn = network.Cellular()
 
 print("- Waiting for the module to be connected to the cellular network... ",
       end="")
 while not conn.isconnected():
-    time.sleep(5)
+    print("ATAI Output: " + xb.atcmd('AI'))
+    print("ATDB Output: " + network.Cellular().signal())
+    time.sleep(10)
 print("[OK]")
 print()
 
-xb = xbee.XBee()
 
+button = Pin.board.D4
+button.mode(Pin.IN)
+
+count = 0
 while conn.isconnected():
-    print("ATAI Output: " + xb.atcmd('AI'))
-    print("ATDB Output: " + network.Cellular().signal())
-    time.sleep(30)
+    count += 1
+    if (count == 10):
+        print("ATAI Output: " + xb.atcmd('AI'))
+        print("ATDB Output: " + network.Cellular().signal())
+        count = 0
+    if (button.value() == 0):
+        print("User button pressed")
+        break
+    time.sleep(1)
 
 print("Program stopped: LTE disconnected from cellular network.")
+
+
